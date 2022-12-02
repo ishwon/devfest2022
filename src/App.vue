583ea4bab35acfb5c8cad02c3f6d0393a0da6d42
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2022-12-02 23:27:09 +0400

    update top URLs

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,25 @@ import HelloWorld from './components/HelloWorld.vue'
   <header class="shadow-md bg-white sticky top-0 z-50">
     <nav class="max-w-6xl mx-auto py-4 px-4 lg:px-0">
         <div class="flex justify-between items-center">
-            <RouterLink to="/">
-                <img src="/images/gdg-mauritius-logo.svg" class="h-3.5 md:h-5" />
-            </RouterLink>
+            <!-- <RouterLink to="/"> -->
+            <a href="https://gdg.community.dev/gdg-mauritius" target="_blank"
+              rel="noreferrer" class="text-sm"><img src="/images/gdg-mauritius-logo.svg" class="h-3.5 md:h-5" /></a>
+            <!-- </RouterLink> -->
 
             <div class="hidden md:block">
               <ul class="flex flex-row gap-4 text-sm">
-                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300"><RouterLink to="/">HOME</RouterLink></li>
-                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300"><RouterLink to="/">AGENDA</RouterLink></li>
-                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300"><RouterLink to="/">SPEAKERS</RouterLink></li>
-                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300"><a href="https://gdg.community.dev/events/details/google-gdg-mauritius-presents-devfest-mauritius-2022/">REGISTER</a></li>
+                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300">
+                  <RouterLink to="/">HOME</RouterLink>
+                </li>
+                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300">
+                  <RouterLink to="/">AGENDA</RouterLink>
+                </li>
+                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300">
+                  <RouterLink to="/">SPEAKERS</RouterLink>
+                </li>
+                <li class="py-2 border-b-4 border-white hover:border-b-4 hover:border-green-600 hover:transition hover:ease-in-out duration-300">
+                  <a href="https://gdg.community.dev/events/details/google-gdg-mauritius-presents-devfest-mauritius-2022/" target="_blank" rel="noreferrer" >REGISTER</a>
+                </li>
               </ul>
             </div>
         </div>
