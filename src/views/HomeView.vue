fc13e2618bb70fb3efe7663fec709f5657c51eb7
Ish Sookun <ish.sookun@lasentinelle.mu>
2022-12-08 13:55:45 +0400

    Re-arranged partner logos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@
                     We are so thrilled to be able to host DevFest Mauritus 2022 at the African Leadership College this year.
                 </p>
                 <p class="mb-2 md:mb-4 text-sm md:text-lg">
-                    Although restrictions are lifted, we highly recommend everyone to take necessary precautions as 
+                    Although restrictions are lifted, we highly recommend everyone to take necessary precautions as
                     COVID is still present. Therefore please consider the following when attending the conference.
                 </p>
                 <p>
@@ -70,7 +70,7 @@
                     This is a gathering of developers and technology enthusiasts who will cover multiple product areas such as Android, Firebase, Kotlin, Flutter, Google Cloud Platform, ARCore, Google Assistant, Design Sprint, IoT, Machine Learning, Mobile Web and Artificial Intelligence.
                 </p>
                 <p class="mb-2 md:mb-4 text-sm md:text-lg">
-                    Key sponsors will be present on that day. Do not hesitate to meet them at their booths to learn more about them. See what they have waiting for you 🙂. 
+                    Key sponsors will be present on that day. Do not hesitate to meet them at their booths to learn more about them. See what they have waiting for you 🙂.
                 </p>
             </div>
         </div>
@@ -87,22 +87,26 @@
 
         <div class="md:hidden sm:block lg:block">
             <div class="flex flex-col lg:flex-row md:gap-4 items-center">
+                <img class="h-12 md:mr-4 mt-10 md:mt-0" src="/images/Google.svg" />
                 <img class="h-32" src="/images/novity.jpg" />
-                <img class="h-32" src="/images/sdworx.svg" />
-                <img class="h-12 md:ml-4 md:mr-6 mt-3 md:mt-0" src="/images/cloud-mu.svg" />
-                <img class="h-32 mt-3 md:mt-0" src="/images/ios.png" />
-                <img class="h-12 md:mx-4 mt-10 md:mt-0" src="/images/Google.svg" />
+                <img class="h-12" src="/images/sdworx.png" />
+                <img class="h-12 md:ml-4 md:mr-6 mt-6 md:mt-0" src="/images/cloud-mu.svg" />
+            </div>
+            <div class="mt-6 flex flex-col lg:flex-row md:gap-4 items-center">
+                <img class="h-12 mt-3 md:mt-0" src="/images/alc_logo.jpg" />
+                <img class="h-28 mt-3 md:mt-0" src="/images/ios.png" />
             </div>
         </div>
 
         <div class="hidden md:block lg:hidden">
             <div class="flex flex-row gap-4 items-center">
+                            <img class="h-12 md:mx-4 mt-10 md:mt-0" src="/images/Google.svg" />
 							<img class="h-32" src="/images/novity.jpg" />
 							<img class="h-32" src="/images/sdworx.svg" />
 							<img class="h-12 md:ml-4 md:mr-6 mt-3 md:mt-0" src="/images/cloud-mu.svg" />
             </div>
             <div class="flex flex-row gap-4 items-center">
-							<img class="h-12 md:mx-4 mt-10 md:mt-0" src="/images/Google.svg" />
+
 							<img class="h-32 mt-3 md:mt-0" src="/images/ios.png" />
             </div>
         </div>
